52ff78e6e41df69a024421e0789a3189cc5abd44
Daniel Cirket <daniel.cirket@gmail.com>
2017-03-12 21:39:55 +0000

    Initial Azure Event Hub project creation

---
+++ src/OpenMessage.Providers.Azure.EventHub/Configuration/AzureEventHubProviderOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenMessage.Providers.Azure.EventHub.Configuration
+{
+    public class AzureEventHubProviderOptions
+    {
+        /// <summary>
+        ///     The connection string to use when connection to the Azure Event Hub Namespace.
+        /// </summary>
+        public string ConnectionString { get; set; }
+        /// <summary>
+        ///     The entity path to use when connection to the Azure Event Hub Namespace.
+        /// </summary>
+        public string EntityPath { get; set; }
+    }
+}
---
+++ src/OpenMessage.Providers.Azure.EventHub/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OpenMessage.Providers.Azure.EventHub
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddOpenMessage(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            // TODO(Dan): Add required services here
+
+            return services;
+        }
+    }
+}
