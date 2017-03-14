e6ac31e6585fbef5e956b65a9eacfb9751050c61
Daniel Cirket <daniel.cirket@gmail.com>
2017-03-14 09:14:46 +0000

    Added event hub event dispatcher

---
+++ src/OpenMessage.Providers.Azure.EventHub/Dispatchers/EventDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenMessage.Providers.Azure.EventHub.Management;
+
+namespace OpenMessage.Providers.Azure.EventHub.Dispatchers
+{
+    internal class EventDispatcher<T> : IDispatcher<T>
+    {
+        private readonly IEventClient<T> _client;
+
+        public EventDispatcher(IEventFactory<T> eventFactory)
+        {
+            if (eventFactory == null)
+                throw new ArgumentNullException(nameof(eventFactory));
+
+            _client = eventFactory.Create();
+        }
+
+        public Task DispatchAsync(T entity, TimeSpan scheduleIn) => _client.SendAsync(entity, scheduleIn);
+    }
+}
