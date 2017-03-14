7a454c90f95e04467731db4bad83b548996fb481
Daniel Cirket <daniel.cirket@gmail.com>
2017-03-14 09:20:30 +0000

    Added serializationprovider interface

---
+++ src/OpenMessage.Providers.Azure.EventHub/Serialization/ISerializationProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.EventHubs;
+using System.IO;
+
+namespace OpenMessage.Providers.Azure.EventHub.Serialization
+{
+    internal interface ISerializationProvider
+    {
+        EventData Serialize<T>(T entity);
+        T Deserialize<T>(EventData entity);
+    }
+}
